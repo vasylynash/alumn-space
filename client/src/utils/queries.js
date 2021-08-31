5160d075bd5ab61bb3df477238da876f841c5235
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-08-30 19:18:54 -0700

    server side auth

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,1 @@
 import { gql } from "@apollo/client";
-
-export const QUERY_ME = gql`
-  query GET_ME {
-    me {
-      _id
-      username
-      email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
-    }
-  }
-`;
