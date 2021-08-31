48996e9ea710f5984a38fef359b4fe94397bca5f
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-08-30 18:41:52 -0700

    auth login signup

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query GET_ME {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
