e9ef8825dfd80aecfe4fc97261ae3c329bc69599
Dmitriy <dmitriy.babich0@gmail.com>
2021-09-02 14:07:09 -0700

    login done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import GlobalStyle from './global.style';
+import { VerticalDiv } from './Landing';
+import { LoginBtn } from './Landing';
+import { SignUpBtn } from './Landing';
+import { BackArrow } from '../components/icons.styles'
+import logo from '../images/logo.png';
+import TextField from '@material-ui/core/TextField';
 
 const Login = () => {
     return ( 
-        <div>
-            <h5>Login</h5>
+        <VerticalDiv>
+            <GlobalStyle/>
+            <Link to='/'>
+                <BackArrow className="fas fa-arrow-left"></BackArrow>
+            </Link>
+            <img src={logo}  alt="logo images" height='200px' width='50%'/>
+            <h1 style={{fontSize:'37px', margin:'0'}}>Hello!</h1>
+            <p style={{fontSize:'15px'}}>Plase <span style={{color:'#51BBB9',fontWeight:'bold'}}>Login </span>to use AlumnSpace</p>
+            <form noValidate autoComplete="off">
+                <VerticalDiv>
+                    <TextField id="standard-basic" label="Username" color='primary' />
+                    <TextField id="password" label="Password" color='primary' type='password' />
+                    <LoginBtn textColor='white' backgroundColor='#51BBB9' type='Submit' style={{marginTop:'20px'}}>Login</LoginBtn>
+                    <Link to='/signup'>
+                        <SignUpBtn textColor='#707070' backgroundColor='#EDEDED'>Sign up</SignUpBtn>
+                    </Link>
+                </VerticalDiv>
+            </form>
 
-        </div>
+        </VerticalDiv>
      );
 }
  
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -41,7 +41,7 @@ const Signup = () => {
             </Link>
             <img src={logo}  alt="logo images" height='200px' width='50%'/>
             <h1 style={{fontSize:'37px', margin:'0'}}>Welcome!</h1>
-            <p style={{fontSize:'15px'}}><span style={{color:'#51BBB9',fontWeight:'bold'}}>Sign up </span>to use AlumnSpace</p>
+            <p style={{fontSize:'15px'}}>Please <span style={{color:'#51BBB9',fontWeight:'bold'}}>Sign up </span>to use AlumnSpace</p>
             <form noValidate autoComplete="off">
                 <VerticalDiv>
                     <TextField id="standard-basic" label="Name" color='primary' />
