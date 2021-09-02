85878cbb373fcacd353fb57fd9d9f09ae189222d
Dmitriy <dmitriy.babich0@gmail.com>
2021-09-01 23:23:22 -0700

    signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,21 +1,106 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import logo from '../images/logo.png';
+import TextField from '@material-ui/core/TextField';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/core/styles';
 // styled components
 import GlobalStyle from './global.style';
 import { VerticalDiv } from './Landing';
 import { LoginBtn } from './Landing';
 import { SignUpBtn } from './Landing';
 
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      paddingRight: 160
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
+
+
+
 
 const Signup = () => {
+
+    // date picker 
+    const [selectedDate, setSelectedDate] = useState(new Date('2021-01-01T21:11:54'));
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+      };
+
+    //   subject picker
+    const classes = useStyles();
+    const [subject, setSubject] = useState();
+
+    const handleSubjectChange = (event) => {
+        setSubject(event.target.value);
+    };
+
     return ( 
         <VerticalDiv>
             <GlobalStyle/>
             <img src={logo}  alt="logo images" height='200px' width='50%'/>
             <h1 style={{fontSize:'37px', margin:'0'}}>Welcome!</h1>
             <p style={{fontSize:'15px'}}><span style={{color:'#51BBB9',fontWeight:'bold'}}>Sign up </span>to use AlumnSpace</p>
-
+            <form noValidate autoComplete="off">
+                <VerticalDiv>
+                    <TextField id="standard-basic" label="Name" color='primary' />
+                    <TextField id="standard-basic" label="Username" color='primary' />
+                    <TextField id="standard-basic" label="Email" color='primary' />
+                    <FormControl style={{minWidth: 190}}>
+                    <InputLabel id="demo-simple-select-label" >Your subject</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={subject}
+                        onChange={handleSubjectChange}
+                        >
+                        <MenuItem value={10}>subject1</MenuItem>
+                        <MenuItem value={20}>subject2</MenuItem>
+                        <MenuItem value={30}>subject3</MenuItem>
+                        <MenuItem value={30}>subject4</MenuItem>
+                        <MenuItem value={30}>subject5</MenuItem>
+                        <MenuItem value={30}>subject6</MenuItem>
+                        <MenuItem value={30}>subject7</MenuItem>
+                        <MenuItem value={30}>subject8</MenuItem>
+                        <MenuItem value={30}>subject9</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <div style={{width:'80%'}}>
+                            <KeyboardDatePicker
+                                margin="normal"
+                                width={1}
+                                id="date-picker-dialog"
+                                label="Date picker dialog"
+                                format="MM/dd/yyyy"
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                                }}
+                            />
+                        </div>
+                    </MuiPickersUtilsProvider>
+                    <TextField id="password" label="Password" color='primary' type='password' />
+                    <TextField id="confirmPassword" label="Confirm password" color='primary' type='password'/>
+                    <SignUpBtn textColor='white' backgroundColor='#51BBB9' type='Submit' style={{marginTop:'20px'}}>Sign up</SignUpBtn>
+                    <LoginBtn textColor='#707070' backgroundColor='#EDEDED'>Login</LoginBtn>
+                </VerticalDiv>
+            </form>
         </VerticalDiv>
      );
 }
