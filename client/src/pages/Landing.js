21cc72d404f9b66e7c6157d9c09dd01627f43c3d
Dmitriy <dmitriy.babich0@gmail.com>
2021-09-01 20:04:00 -0700

    signup top half done

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 import styled from 'styled-components';
+import GlobalStyle from './global.style';
 
 // styles
 
-const VerticalDiv = styled.div`
+ export const VerticalDiv = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -17,7 +18,7 @@ const Title = styled.h1`
     margin: 0;
 `
 
-const LoginBtn = styled.button`
+export const LoginBtn = styled.button`
     font-family: 'Montserrat', sans-serif;
     font-weight: bold;
     font-size: 15px;
@@ -34,7 +35,7 @@ const LoginBtn = styled.button`
     }
 `
 
-const SignUpBtn = styled.button`
+export const SignUpBtn = styled.button`
     font-family: 'Montserrat', sans-serif;
     font-weight: bold;
     font-size: 15px;
@@ -56,7 +57,8 @@ const SignUpBtn = styled.button`
 
 const Landing = () => {
     return ( 
-        <VerticalDiv> 
+        <VerticalDiv>
+            <GlobalStyle/> 
             <img src={logo}  alt="logo images" height='20%' width='90%'/>
             <Title>AlumSpace</Title>
             <p style={{textAlign:'center', fontFamily: 'Montserrat, sans-serif'}}> A place for coding bootcamp Alumni to connect  with each other <br/> and share ideas.</p>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import logo from '../images/logo.png';
+// styled components
+import GlobalStyle from './global.style';
+import { VerticalDiv } from './Landing';
+import { LoginBtn } from './Landing';
+import { SignUpBtn } from './Landing';
+
 
 const Signup = () => {
     return ( 
-        <div>
-            <h5>Sign Up</h5>
-        </div>
+        <VerticalDiv>
+            <GlobalStyle/>
+            <img src={logo}  alt="logo images" height='200px' width='50%'/>
+            <h1 style={{fontSize:'37px', margin:'0'}}>Welcome!</h1>
+            <p style={{fontSize:'15px'}}><span style={{color:'#51BBB9',fontWeight:'bold'}}>Sign up </span>to use AlumnSpace</p>
+
+        </VerticalDiv>
      );
 }
  
