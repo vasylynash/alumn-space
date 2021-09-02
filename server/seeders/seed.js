c749cd464c5056e17136f18102e7e18ff8db388b
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-09-01 18:09:39 -0700

    Seeds (#9)
    * seeds setup
    
    * seeds setup
    
    * seeds setup
    
    * seeds setup
    
    * seeds with seeders

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,2 +1,22 @@
-const db = require('../config/connection');
+const db = require("../config/connection");
+const { Category, Comment, Label, Post, User } = require("../models");
+const categorySeed = require("./categorySeed.json");
+const commentSeed = require("./commentSeed.json");
+const labelSeed = require("./labelSeed.json");
+const postSeed = require("./postSeed.json");
+const userSeed = require("./userSeed.json");
 
+db.once("open", async () => {
+  try {
+    await Category.create(categorySeed);
+    await Comment.create(commentSeed);
+    await Label.create(labelSeed);
+    await Post.create(postSeed);
+    await User.create(userSeed);
+
+    console.log("Seeds are deployed!");
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
