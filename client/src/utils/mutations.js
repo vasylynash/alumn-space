a4d14ce955fc67579953dbb011e660dca0e1297b
Melina Boedecker <melinamboedecker@gmail.com>
2021-09-01 19:08:30 -0700

    added mutations.js in front end, and fixed missing line 3 in Signup.js (#11)
    * added mutations.js in front end, and fixed missing line 3 in Signup.js
    
    * changed firstName and lastName to camelCase

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,43 @@
 import { gql } from '@apollo/client';
 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!, yearOfGraduation: String!, class: String!) {
+    addUser(username: $username, email: $email, password: $password, yearOfGraduation: $yearOfGraduation, class: $class) {
+      token
+      user {
+          _id
+          username
+          email
+          firstName
+          lastName
+          image
+          registrationDate
+          role
+          bio
+          yearOfGraduation
+          linkedIn
+          gitHub
+          class
+          posts {
+              _id
+              title
+              body
+              author
+              dateCreated
+              likes
+              comments {
+                  _id
+                  body
+                  author
+                  dateCreated
+                  likes
+              }
+          }
+      }
+    }
+  }
+`;
+
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -10,5 +48,118 @@ export const LOGIN_USER = gql`
       }
     }
   }
+`;
+
+export const ADD_POST = gql`
+  mutation addPost($title: String!, $body: String!, $author: String!) {
+      addPost(title: $title, body: $body, author: $author) {
+          _id
+          title
+          body
+          author
+          dateCreated
+          likes
+          comments {
+              _id
+              body
+              author
+              dateCreated
+              likes
+          }
+      }
+  }
+  `;
+
+export const UPDATE_POST = gql`
+    mutation updatePost($postId: ID!, $title: $String!, $body: String!) {
+        updatePost(postId: $postId, title: $title, body: $body) {
+            _id
+            title
+            body
+        }
+    }
+    `;
+
+export const REMOVE_POST = gql`
+        mutation removePost($postId: ID!) {
+            removePost(postId: $postId) {
+               _id
+               posts {
+                   _id
+                   title
+                   body
+                   author
+                   dateCreated
+                   likes
+                   comments {
+                       _id
+                       body
+                       author
+                       dateCreated
+                       likes
+                   }
+               } 
+            }
+        }
+        `;
+
+export const ADD_COMMENT = gql`
+        mutation addComment(postId: ID!, commentText: String!) {
+            addComment(postId: $postId, commentText: $commentText) {
+                _id
+                title
+                body
+                author
+                dateCreated
+                likes
+                comments {
+                    _id
+                    body
+                    author
+                    dateCreated
+                    likes
+                }
+            }
+        }
+`;
+
+export const UPDATE_COMMENT = gql`
+        mutation updateComment(commentId: ID!, commentText: String!) {
+            updateComment(commentId: $commentId, commentText: $commentText) {
+                _id
+                title
+                body
+                author
+                dateCreated
+                likes
+                comments {
+                    _id
+                    body
+                    author
+                    dateCreated
+                    likes
+                }
+            }
+        }
+`;
+
+export const REMOVE_COMMENT = gql`
+        mutation removeComment(commentId: ID!) {
+            removeComment(commentId: $commentId) {
+                _id
+                title
+                body
+                author
+                dateCreated
+                likes
+                comments {
+                    _id
+                    body
+                    author
+                    dateCreated
+                    likes
+                }
+            }
+        }
 `;
 
