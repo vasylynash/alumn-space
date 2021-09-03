55d2bbd556091784c981f20cf472d5be6948e93b
Dmitriy <dmitriy.babich0@gmail.com>
2021-09-03 13:01:59 -0700

    removed replaced react logos and added the hamburger for the nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Header from '../components/Header';
+import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar';
 
 
@@ -8,7 +8,7 @@ const Home = () => {
     
     return ( 
         <div>
-            <Header />
+            <Navbar/>
             
             <Sidebar />
         </div>
