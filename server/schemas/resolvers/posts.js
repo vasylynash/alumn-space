872b066c47ddb8e2f77c830e27695e50934cc057
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-09-05 13:31:58 -0700

    added likes resolver

--- server/schemas/resolvers/posts.js
+++ server/schemas/resolvers/posts.js
@@ -1,14 +1,13 @@
-const Post = require('../../models/Post');
-const { UserInputError } = require('apollo-server-express');
+const Post = require("../../models/Post");
+const { UserInputError } = require("apollo-server-express");
 
 module.exports = {
-    Query: {
+  Query: {
     posts: async () => {
       try {
-        return Post.find();        
-      }
-      catch(err) {
-        throw new Error(err)
+        return Post.find();
+      } catch (err) {
+        throw new Error(err);
       }
     },
     post: async (_, args) => {
@@ -17,35 +16,40 @@ module.exports = {
         if (post) {
           return post;
         } else {
-          throw new Error('Post not found');
+          throw new Error("Post not found");
         }
+      } catch (err) {
+        throw new Error(err);
       }
-      catch (err) {
-        throw new Error(err)
-      }
-    }
+    },
   },
   Mutation: {
     addPost: async (_, { title, body, author }) => {
-      return Post.create({title: title, body: body, author: author});
+      return Post.create({ title: title, body: body, author: author });
     },
     updatePost: async (_, { id, title, body }) => {
-      if(!title) {
+      if (!title) {
         throw new UserInputError("The title cannot be empty");
       }
-      if(!body) {
+      if (!body) {
         throw new UserInputError("The body of the post cannot be empty");
       }
-      const post = await Post.findById({_id: id});
+      const post = await Post.findById({ _id: id });
       post.title = title;
       post.body = body;
-      
+
       return post;
     },
-    removePost: async (_, args)  => {
+    removePost: async (_, args) => {
       const id = args.id;
       Post.findByIdAndRemove(id);
       return id;
-    } 
-  }    
+    },
+    addCommentLike: async (_, { likes, comments }) => {
+      return (Post.comments.likes += 1);
+    },
+    addPostLike: async (_, { likes }) => {
+      return (Post.likes += 1);
+    },
+  },
 };
