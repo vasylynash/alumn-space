adf100a765b5ecc5d7ab7a1dfff351777cfc07d1
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-09-05 15:01:08 -0700

    added separate Likes resolver

--- server/schemas/resolvers/likes.js
+++ server/schemas/resolvers/likes.js
@@ -3,11 +3,11 @@ const Post = require("../../models/Post");
 module.exports = {
   Query: {
     addCommentLike: async (_, { comments }) => {
-      comments.likes = Post.comments.likes + 1;
+      // comments.likes = Post.comments.likes + 1;
       return comments.likes;
     },
     addPostLike: async (_, { likes }) => {
-      likes = Post.likes + 1;
+      // likes = Post.likes + 1;
       return likes;
     },
   },
