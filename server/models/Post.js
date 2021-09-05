8a990271a0cf621eda26ece20815ba09eb15a1d6
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-09-05 11:20:37 -0700

    user refs

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,60 +1,59 @@
-const { Schema, model, now } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model, now } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const postSchema = new Schema({
-    title: {
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  comments: [
+    {
+      commentText: {
         type: String,
-        required: true
-    },
-    body: {
+        required: true,
+        minlength: 1,
+        maxlength: 500,
+      },
+      author: {
         type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    comments: [
-        {
-            commentText: {
-                type: String,
-                required: true,
-                minlength: 1,
-                maxlength: 500
-            },
-            author: {
-                type: String,
-                required: true
-            },
-            dateCreated: {
-                type: Date,
-                default: Date.now,
-                get: (timestamp) => dateFormat(timestamp),
-            },
-            likes: {
-                type: Number
-            }
-        }
-    ],
-    dateCreated: {
+        required: true,
+      },
+      dateCreated: {
         type: Date,
         default: Date.now,
         get: (timestamp) => dateFormat(timestamp),
-    },
-    likes: {
+      },
+      likes: {
         type: Number,
+      },
     },
-    category: {
-        type: Schema.Types.ObjectId,
-        ref: 'Category'
-    },
-    label: {
-        type: Schema.Types.ObjectId,
-        ref: 'Label'
-    }
-
+  ],
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  likes: {
+    type: Number,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  label: {
+    type: Schema.Types.ObjectId,
+    ref: "Label",
+  },
 });
 
-const Post = model('Post', postSchema);
+const Post = model("Post", postSchema);
 
 module.exports = Post;
