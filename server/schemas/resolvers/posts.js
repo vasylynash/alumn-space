912b78041643220e5796bd75fea317c3ae49cabc
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-09-05 14:40:21 -0700

    added likes types

--- server/schemas/resolvers/posts.js
+++ server/schemas/resolvers/posts.js
@@ -46,13 +46,13 @@ module.exports = {
       return id;
     },
     //--
-    // addCommentLike: async (_, { comments }) => {
-    //   comments.likes = Post.comments.likes + 1;
-    //   return comments.likes;
-    // },
-    // addPostLike: async (_, { likes }) => {
-    //   likes = Post.likes + 1;
-    //   return likes;
-    // },
+    addCommentLike: async (_, { comments }) => {
+      comments.likes = Post.comments.likes + 1;
+      return comments.likes;
+    },
+    addPostLike: async (_, { likes }) => {
+      likes = Post.likes + 1;
+      return likes;
+    },
   },
 };
