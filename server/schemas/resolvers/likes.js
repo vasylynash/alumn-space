56524039f7e2633b5de26650085e128386d81116
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-09-07 13:42:05 -0700

    Update likes.js

--- server/schemas/resolvers/likes.js
+++ server/schemas/resolvers/likes.js
@@ -4,8 +4,8 @@ module.exports = {
   Query: {
     postLikes: async (_, args) => {
        const post = await Post.findById(args.id);
-       post.totalLikes = post.likes.length
-      return await post
+       post.totalLikes = post.likes.length;
+      return await post;
     },
   },
 
@@ -14,9 +14,9 @@ module.exports = {
       const post = await Post.findById(args.id);
 
       if (!post.likes.includes(args.userId)){
-      post.likes.push(args.userId)
-      post.totalLikes = post.likes.length
-      await Post.updateOne(post)
+      post.likes.push(args.userId);
+      post.totalLikes = post.likes.length;
+      await Post.updateOne(post);
       
       }
   
