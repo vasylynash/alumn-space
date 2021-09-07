ab48b0f6335c08f588ee543d18e78a0a5f284a99
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-09-06 17:24:45 -0700

    front end queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -166,8 +166,7 @@ export const REMOVE_COMMENT = gql`
 export const ADD_POST_LIKE = gql`
         mutation addPostLike(id: ID!, userId: ID! ){
             addPostLike(id: $postId, userId: $userId){
-                _id
-                likes
+                totalLikes
             }
         }
 `;
