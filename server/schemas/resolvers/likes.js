75a923b48fe0d667c25ca5d15d457b0abb4b4718
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-09-12 17:53:50 -0700

    added likeing functions refething data (#62)
    * added likeing functions refething data
    
    * added remove like

--- server/schemas/resolvers/likes.js
+++ server/schemas/resolvers/likes.js
@@ -3,16 +3,16 @@ const Post = require('../../models/Post');
 module.exports = {
 
   Mutation: {
-    addPostLike: async (_, args) => {
-      const post = await Post.findById(args.id);
-
-      if (!post.likes.includes(args.userId)){
-      post.likes.push(args.userId);
+    addPostLike: async (_,{ postId, userId }) => {
+      const post = await Post.findById(postId);
+      if (!post.likes.includes(userId)){
+      post.likes.push(userId);
       post.totalLikes = post.likes.length;
-      await Post.updateOne(post);
-      
+      } else {
+       post.likes = post.likes.filter(item => item !== userId)
+       post.totalLikes = post.likes.length;
       }
-  
+      await Post.updateOne({ "_id": postId},{"totalLikes":post.totalLikes,"likes":post.likes});
       return post;
     },
   },
