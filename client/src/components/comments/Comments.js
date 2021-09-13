c217ad826235df91a0fd9a843e695144aa4fbb6d
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-09-12 17:03:14 -0700

    comments page population

--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import { VerticalDiv } from '../../pages/Landing'
-import GlobalStyle from '../global.style'
-import Navbar from '../nav/Navbar'
-import { Line, SearchBar, SearchBtn, SearchIcon, SearchInput } from '../posts/SearchPost'
-import Comment from '../comments/UserComment'
-import { BackArrow } from '../icons.styles'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { VerticalDiv } from '../../pages/Landing';
+import GlobalStyle from '../global.style';
+import Navbar from '../nav/Navbar';
+import { Line, SearchBar, SearchBtn, SearchIcon, SearchInput } from '../posts/SearchPost';
+import Comment from '../comments/UserComment';
+import { BackArrow } from '../icons.styles';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { QUERY_SINGLE_POST } from '../../utils/queries';
+import { useQuery } from '@apollo/client';
+const  Comments = () => {
+    const { postId } = useParams();
 
-function Comments() {
-    return (
+    const { loading, error, data } = useQuery(QUERY_SINGLE_POST, {
+        variables: { id: postId },
+      });
+      const {comments} = data?.post || {};
+  
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+  
+   return (
         <>
         <GlobalStyle/>
         <Navbar/>
@@ -17,10 +30,13 @@ function Comments() {
         </Link>
             <VerticalDiv>
                 <h1 style={{fontSize:'39px', marginTop:'0.5rem'}}>Comments</h1>
-                <Comment/>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {
+                comments.map(comment=>{
+                    return (
+                        <Comment comment={comment} />
+                    )
+                })
+                }
                 <SearchBar>
                     <SearchIcon className='far fa-comment-alt'/>
                     <SearchInput placeholder='Add Comment'/>
