6698f788a45786ff6a87a7d771beb997fd4f9c3c
Melina Boedecker <melinamboedecker@gmail.com>
2021-09-13 13:00:45 -0700

    Profileconnecttobackend (#64)
    * updated queries.js in frontend to fix typos and add euery single category and label, updated mutation for ADD_USER with registerInput, and updated all comment body to commentText
    
    * changed date picker to year only and connected form to back end
    
    * changed maxlength for title to 20 in Post model
    
    * creating formState
    
    * added class names and datepicker for year
    
    * add console.log
    
    * add handleChange
    
    * added user id
    
    * update user profile
    
    * added username to profile page
    
    * added password to updateUser resolver
    
    * added email to mutation and type defs for updating user profile so that email can be updated

--- client/src/components/profile/Info.js
+++ client/src/components/profile/Info.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { MuiPickersUtilsProvider, KeyboardDatePicker, DatePicker } from '@material-ui/pickers';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -10,54 +10,91 @@ import Select from '@material-ui/core/Select';
 import { LoginBtn, VerticalDiv } from '../../pages/Landing';
 import GlobalStyle from '../global.style';
 
+import {  useMutation } from '@apollo/client';
+import { UPDATE_USER_PROFILE } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
+const user = Auth.getProfile().data
 
 const Info = () => {
 
-    const [selectedDate, setSelectedDate] = useState(new Date('2021-01-01T21:11:54'));
+    const [formState, setFormState] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        className: '', 
+        bio: ''
+    })
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-      };
+    const [selectedDate, handleDateChange] = useState(new Date());
 
-    const [subject, setSubject] = useState();
+    const [updateUser, { error }] = useMutation(UPDATE_USER_PROFILE);
 
-    const handleSubjectChange = (event) => {
-        setSubject(event.target.value);
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log('selectedDate ->    ', selectedDate)
+        console.log('formState ->    ', formState)
+        console.log('email ->    ', formState.email)
+
+        try {
+            const mutationResponse = await updateUser ({
+                variables: {
+                    id: user._id,
+                    firstName: formState.firstName,
+                    lastName: formState.lastName,
+                    email: formState.email,
+                    className: formState.className,
+                    yearOfGraduation: selectedDate,
+                    linkedIn: formState.linkedIn,
+                    gitHub: formState.gitHub,
+                    bio: formState.bio
+                },
+            });
+
+        } catch (e) {
+            console.error(e);
+        }
     };
+    
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    }; 
 
     return ( 
         <VerticalDiv>
             <GlobalStyle/>
-            <form noValidate autoComplete="off">
+            <form noValidate autoComplete="off" onSubmit={handleFormSubmit}>
                 <VerticalDiv>
                 <h1 style={{fontSize:'25px', margin:'0', color:'#51BBB9'}}>My information</h1>
                 <p style={{fontSize:'12px', color:'grey'}}>Update your info</p>
-                    <TextField id="firstName" label="First Name" color='primary' />
-                    <TextField id="lastName" label="Last Name" color='primary' />
-                    <TextField id="email" label="Email" color='primary' />
+                    <TextField id="firstName" label="First Name" color='primary' name="firstName" onChange={handleChange} />
+                    <TextField id="lastName" label="Last Name" color='primary' name="lastName" onChange={handleChange} />
+                    <TextField id="email" label="Email" color='primary' name="email" onChange={handleChange} />
                     <FormControl style={{minWidth: 190}}>
-                    <InputLabel id="subject" >Your subject</InputLabel>
+                    <InputLabel id="subject" >Your class</InputLabel>
                         <Select
                         labelId="subject"
                         id="subject-select"
-                        value={subject}
-                        onChange={handleSubjectChange}
+                        name="className"
+                        // value={subject}
+                        onChange={handleChange}
                         >
-                        <MenuItem value={1}>subject1</MenuItem>
-                        <MenuItem value={2}>subject2</MenuItem>
-                        <MenuItem value={3}>subject3</MenuItem>
-                        <MenuItem value={4}>subject4</MenuItem>
-                        <MenuItem value={5}>subject5</MenuItem>
-                        <MenuItem value={6}>subject6</MenuItem>
-                        <MenuItem value={7}>subject7</MenuItem>
-                        <MenuItem value={8}>subject8</MenuItem>
-                        <MenuItem value={9}>subject9</MenuItem>
+                        <MenuItem value={'Web Development'}>Web Development</MenuItem>
+                        <MenuItem value={'Data Analytics'}>Data Analytics</MenuItem>
+                        <MenuItem value={'UX/UI Design'}>UX/UI Design</MenuItem>
+                        <MenuItem value={'Cybersecurity'}>Cybersecurity</MenuItem>
+                        <MenuItem value={'FinTech'}>FinTech</MenuItem>
+                        <MenuItem value={'Digital Marketing'}>Digital Marketing</MenuItem>
                         </Select>
                     </FormControl>
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
                         <div style={{width:'80%'}}>
-                            <KeyboardDatePicker
+                            {/* <KeyboardDatePicker
                                 margin="normal"
                                 width={1}
                                 id="date-picker-dialog"
@@ -68,15 +105,25 @@ const Info = () => {
                                 KeyboardButtonProps={{
                                 'aria-label': 'change date',
                                 }}
-                            />
+                            /> */}
+                            <DatePicker
+                                views={["year"]}
+                                label="Year graduated"
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                />
                         </div>
                     </MuiPickersUtilsProvider>
+                    <TextField id="linkedin" label="LinkedIn" color='primary' name="linkedIn"  onChange={handleChange} />
+                    <TextField id="github" label="GitHub" color='primary' name="gitHub" onChange={handleChange} />
                     <TextField
                     className='input'
                     id="standard-multiline-static"
                     label="Bio"
                     multiline
                     rows={2}
+                    name="bio"
+                    onChange={handleChange}
                     />
                     <LoginBtn type='submit'>Update</LoginBtn>
                 </VerticalDiv>
