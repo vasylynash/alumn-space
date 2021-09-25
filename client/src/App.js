a45906b8c394f03758fbb7a24ce14c2af115e17d
Melina Boedecker <melinamboedecker@gmail.com>
2021-09-25 07:50:22 -0700

    changed httpLink back to work with heroku in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,8 @@ import EditPost from './components/post/EditPost';
 import Auth from './utils/auth';
 
 const httpLink = createHttpLink({
-  // uri: '/graphql',
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
+  // uri: 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
